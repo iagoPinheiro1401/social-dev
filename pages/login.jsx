f310524ee21d7e83a2200c6a9dcfc655d0546fce
IagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2023-07-26 10:40:43 -0300

    Criado componente de imagem

---
+++ pages/login.jsx
@@ -0,0 +1,11 @@
+import ImageSpace from "../src/components/layout/ImageWithSpace"
+
+function LoginPage () {
+    return (
+        <ImageSpace>
+            teste
+        </ImageSpace>
+    )
+}
+
+export default LoginPage
