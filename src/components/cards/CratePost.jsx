775e20cf003d2684061c7eb96ffbb06e65a8c617
IagoPinheiro1401 <iago.pinheirodeoliveira@gmail.com>
2023-08-10 22:18:58 -0300

    utilizado sessão no frontEnd e criado logout

--- src/components/cards/CratePost.jsx
+++ src/components/cards/CratePost.jsx
@@ -32,10 +32,10 @@ const BottomContainer = styled.div`
     }
 `
 
-function CreatePost () {
+function CreatePost ({ username }) {
     return (
         <PostContainer  >
-            <H4><Title>No que você está pensando, @nick?</Title></H4>
+            <H4><Title>No que você está pensando, @{username}?</Title></H4>
             <TextContainer>
                 <Textarea placeholder="Digite sua mensagem" rows="4"/>
             </TextContainer>
